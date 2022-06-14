e4b73c4be44bcc1e12479270ca1e7486e74870b8
Maximo-4ever <maximilianocm06@gmail.com>
2022-06-14 14:18:56 -0500

    Agrega loader de espera

---
+++ src/components/Loader/index.jsx
@@ -0,0 +1,16 @@
+import styles from "./styles.module.css";
+
+const Loader = () => {
+  return (
+    <div className={styles.loaderContainer}>
+      <div className={styles.loader}>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
